a36dde62205867527e77bc880359445fcfba8823
ziad mahmoud <zm006463@gmail.com>
2023-12-07 20:23:07 +0200

    dsadasd

---
+++ app.java
@@ -0,0 +1,8 @@
+public class app{
+     public static void main(String[] args){
+         System.out.println("suiiiiiiii");
+         System.out.println("sssss");
+         System.out.println("zswdwd");
+         
+     }
+}
