05588d5392704b7e6255fdbc61cd9b892e8dd92b
ziad mahmoud <zm006463@gmail.com>
2023-12-07 19:12:47 +0200

    add mm

--- index.java
+++ index.java
@@ -2,6 +2,7 @@ public class index{
      public static void main(String[] args){
          System.out.println("suiiiiiiii");
          System.out.println("sssss");
+         System.out.println("zswdwd");
          
      }
 }
